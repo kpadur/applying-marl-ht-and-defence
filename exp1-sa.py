538258babcaec012db2dcf0fa2ac08128eb1e62f
kpadur <31754805+kpadur@users.noreply.github.com>
2025-03-21 16:23:00 +0200

    Update exp1-sa.py

--- exp1-sa.py
+++ exp1-sa.py
@@ -25,23 +25,22 @@ experiment = 1
 
 # %% [markdown]
 # Specify output directory
-save_data_path = os.path.join("sensitivity_analysis", "exp1-results")
-save_figures_path = os.path.join("sensitivity_analysis", "exp1-figures")
+save_data_path = os.path.join("sensitivity-analysis", "exp1-results")
 get_nn_path = os.path.join("regagent-parameters")
-param_values_path = os.path.join("sensitivity_analysis", "param_values")
+param_values_path = os.path.join("sensitivity-analysis", "param-values")
 
 # %% [markdown]
 # Sensitivity analysis function
 def sensitivity_analysis(trial_params):
     # Set hyperparameters (use trained agents)
-    df = pd.read_csv("hyperparameters.csv")
+    df = pd.read_csv("data/hyperparameters.csv")
     hyperparameters = dict(zip(df['hyperparameter'], df['value']))
 
     alpha_rnn1 = hyperparameters['alpha_1']
     alpha_rnn2 = hyperparameters['alpha_2']
 
     # Initialise seed for reproducibility
-    seed = 5282
+    seed = 0
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -58,7 +57,7 @@ def sensitivity_analysis(trial_params):
     nMalAgents = 0
 
     # Parameters
-    # order: 'kappa', 'rho', 'center_up_to_down', 'center_down_to_up', 'end_up_to_down', 'end_down_to_up', 'cost', 'direct_exp_weight', 'satisfaction_threshold', 'forgetting_factor'
+    # order: 'kappa', 'rho', 'center_up_to_down', 'center_down_to_up', 'end_up_to_down', 'end_down_to_up', 'cost', 'direct_exp_weight', 'feedback_adj_rate', 'forgetting_factor'
     kappa = int(trial_params[0])
     rho = trial_params[1]
     center_up_to_down = [trial_params[2]] * nProviders # Psi: prob (1 to -1)
@@ -67,15 +66,15 @@ def sensitivity_analysis(trial_params):
     end_down_to_up = [trial_params[5]] * nProviders # lambda: prob (-1 to 1)
     cost = [trial_params[6]] * nProviders
     direct_exp_weight = trial_params[7]
-    satisfaction_threshold = trial_params[8]
+    feedback_adj_rate = trial_params[8]
     forgetting_factor = trial_params[9]
     print("Running experiment with the following parameters: kappa", kappa, "rho", rho, "center_up_to_down", center_up_to_down, "center_down_to_up", center_down_to_up, "end_up_to_down",
-          end_up_to_down, "end_down_to_up", end_down_to_up, "cost", cost, "direct_exp_weight", direct_exp_weight, "satisfaction_threshold", satisfaction_threshold, "forgetting_factor", forgetting_factor)
+          end_up_to_down, "end_down_to_up", end_down_to_up, "cost", cost, "direct_exp_weight", direct_exp_weight, "satisfaction_threshold", feedback_adj_rate, "forgetting_factor", forgetting_factor)
    
     # Create environment
-    env = Environment(nRegAgents, nMalAgents, nProviders, 
+    env = Environment(nRegAgents, nProviders, 
                         kappa, rho, center_up_to_down, center_down_to_up, end_up_to_down, end_down_to_up, cost, 
-                        direct_exp_weight, satisfaction_threshold, forgetting_factor)
+                        direct_exp_weight, feedback_adj_rate, forgetting_factor)
     
     # Pick one agent randomly from each type of agents
     regagent_example = np.random.choice(env.regagents)
@@ -95,12 +94,12 @@ def sensitivity_analysis(trial_params):
 
     for agent_name, agent in regular_agents.items():
         # Load Action NN and its optimizer
-        action_checkpoint = torch.load(os.path.join(get_nn_path, f'{agent_name}_checkpoint_actions_2024-06-24_5281.pth'))
+        action_checkpoint = torch.load(os.path.join(get_nn_path, f'{agent_name}_checkpoint_actions.pth'))
         agent.action_nn.load_state_dict(action_checkpoint['actions_state_dict'])
         agent.action_opt.load_state_dict(action_checkpoint['actions_opt_state_dict'])
 
         # Load Opinion NN and its optimizer
-        opinion_checkpoint = torch.load(os.path.join(get_nn_path, f'{agent_name}_checkpoint_opinions_2024-06-24_5281.pth'))
+        opinion_checkpoint = torch.load(os.path.join(get_nn_path, f'{agent_name}_checkpoint_opinions.pth'))
         agent.opinion_nn.load_state_dict(opinion_checkpoint['opinions_state_dict'])
         agent.opinion_opt.load_state_dict(opinion_checkpoint['opinions_opt_state_dict'])
 
@@ -108,7 +107,7 @@ def sensitivity_analysis(trial_params):
     total_rewards, action_rewards, opinion_rewards = np.zeros(number_of_episodes + 1),  np.zeros(number_of_episodes + 1), np.zeros(number_of_episodes + 1)
     social_trust_history = np.zeros((number_of_episodes + 1, nProviders))
     actions_history, opinions_history = np.zeros((number_of_episodes + 1, nProviders)), np.zeros((number_of_episodes + 1, nProviders*2))
-    sum_sp_availability = [0, 0, 0]
+    sum_sp_availability = np.zeros(len(nProviders), dtype=int)
     sp_availability_history = np.zeros((number_of_episodes + 1, nProviders))
 
     # Train and evaluate the agent
@@ -133,10 +132,11 @@ def sensitivity_analysis(trial_params):
                 action, opinion = agent.sample_actions(state)
                 actions[agent_name] = (action, opinion) # save action and opinion
             
-            # Service providers provide service
-            for endpoint in env.endpoint.values():
-                for provider in env.providers:
-                    sum_sp_availability[provider] += endpoint[provider]
+            # Service providers service availability
+            for agent_name, (action, _) in actions.items():            
+                # Check if the endpoint for the requested provider is available
+                if env.endpoint[agent_name][action] == 1:
+                    sum_sp_availability[action] += 1
 
             # Perform actions, determine next state, reward, and termination
             observations, rewards, _, _, _ = env.step(actions)
@@ -147,28 +147,26 @@ def sensitivity_analysis(trial_params):
                 all_observations[agent_name].append(observations[agent_name])
                 all_actions[agent_name].append(actions[agent_name])
 
-        # Process regagent rewards
+         # Process regagent rewards
         sum_regagent_rewards, service, feedback = process_regagent_rewards(episode_rewards)
         total_rewards[episode] = sum_regagent_rewards
         action_rewards[episode] = service
         opinion_rewards[episode] = feedback
         # Process social trust
-        episode_states_pd = process_regagent_states_average(all_observations, env.providers, episode, n_steps)
-        social_trust_history[episode][0] = episode_states_pd["trust_in_sp0"].mean()
-        social_trust_history[episode][1] = episode_states_pd["trust_in_sp1"].mean()
-        social_trust_history[episode][2] = episode_states_pd["trust_in_sp2"].mean()
+        # Process social trust
+        mean_trust_values = process_regagent_states_average(all_observations, env.providers, n_steps)
+        social_trust_history[episode] = mean_trust_values
         # Processing regagent actions and opinions
-        _, regagent_action_counts, regagent_opinion_counts = process_regagent_actions(all_actions, env.regagents, episode, n_steps)
-        actions_history[episode] = regagent_action_counts # add occurrences of each action (as %)
-        opinions_history[episode] = regagent_opinion_counts # add occurrences of each opinion (as %)
+        mean_selection_rate, mean_expression_rate = process_regagent_actions(all_actions, env.providers, n_steps)
+        actions_history[episode] = mean_selection_rate # add occurrences of each action (as %)
+        opinions_history[episode] = mean_expression_rate # add occurrences of each opinion (as %)
         # Process service provider availability
-        sp_availability_episode = process_service_provider_availability(nRegAgents, n_steps, sum_sp_availability)
-        for provider in env.providers:
-            sp_availability_history[episode][provider] = sp_availability_episode[provider]
-        sum_sp_availability = [0, 0, 0]
+        sp_availability_episode = process_service_provider_availability(all_actions, env.providers, sum_sp_availability)
+        sp_availability_history[episode] = sp_availability_episode
+        sum_sp_availability = np.zeros(len(env.providers), dtype=int) # reset count to zero
     
     # Log the trial results to the CSV file
-    log_to_csv(kappa, rho, center_up_to_down, center_down_to_up, end_up_to_down, end_down_to_up, cost, direct_exp_weight, satisfaction_threshold, forgetting_factor,
+    log_to_csv(kappa, rho, center_up_to_down, center_down_to_up, end_up_to_down, end_down_to_up, cost, direct_exp_weight, feedback_adj_rate, forgetting_factor,
                np.mean(total_rewards), np.mean(action_rewards), np.mean(opinion_rewards), np.mean(social_trust_history[:,0]), np.mean(social_trust_history[:,1]), np.mean(social_trust_history[:,2]),
                np.mean(actions_history[:,0]), np.mean(actions_history[:,1]), np.mean(actions_history[:,2]), np.mean(opinions_history[:,0]), 
                np.mean(opinions_history[:,1]), np.mean(opinions_history[:,2]), np.mean(opinions_history[:,3]), np.mean(opinions_history[:,4]), np.mean(opinions_history[:,5]), 
@@ -176,7 +174,7 @@ def sensitivity_analysis(trial_params):
 
 # %% [markdown]
 # Log trial results to csv file
-def log_to_csv(kappa, rho, center_up_to_down, center_down_to_up, end_up_to_down, end_down_to_up, cost, direct_exp_weight, satisfaction_threshold, forgetting_factor,
+def log_to_csv(kappa, rho, center_up_to_down, center_down_to_up, end_up_to_down, end_down_to_up, cost, direct_exp_weight, feedback_adj_rate, forgetting_factor,
                total_rewards, action_rewards, opinion_rewards, sp0_social_trust, sp1_social_trust, sp2_social_trust, 
                sp0_request_rate, sp1_request_rate, sp2_request_rate, op0_expression_rate, 
                op1_expression_rate, op2_expression_rate, op3_expression_rate, op4_expression_rate, op5_expression_rate, 
@@ -187,11 +185,11 @@ def log_to_csv(kappa, rho, center_up_to_down, center_down_to_up, end_up_to_down,
         writer = csv.writer(f)
         if not file_exists:
             # Write the header if the file doesn't exist
-            writer.writerow(['kappa', 'rho', 'center_up_to_down', 'center_down_to_up', 'end_up_to_down', 'end_down_to_up', 'cost', 'direct_exp_weight', 'satisfaction_threshold', 'forgetting_factor',
+            writer.writerow(['kappa', 'rho', 'center_up_to_down', 'center_down_to_up', 'end_up_to_down', 'end_down_to_up', 'cost', 'direct_exp_weight', 'feedback_adj_rate', 'forgetting_factor',
                'total_rewards', 'action_rewards', 'opinion_rewards', 'sp0_social_trust', 'sp1_social_trust', 'sp2_social_trust', 'sp0_request_rate', 'sp1_request_rate', 'sp2_request_rate',
                 'op0_expression_rate', 'op1_expression_rate', 'op2_expression_rate', 'op3_expression_rate', 'op4_expression_rate', 'op5_expression_rate', 'sp0_availability', 'sp1_availability', 'sp2_availability'])
         # Write the trial data
-        writer.writerow([kappa, rho, center_up_to_down, center_down_to_up, end_up_to_down, end_down_to_up, cost, direct_exp_weight, satisfaction_threshold, forgetting_factor,
+        writer.writerow([kappa, rho, center_up_to_down, center_down_to_up, end_up_to_down, end_down_to_up, cost, direct_exp_weight, feedback_adj_rate, forgetting_factor,
                total_rewards, action_rewards, opinion_rewards, sp0_social_trust, sp1_social_trust, sp2_social_trust, sp0_request_rate, sp1_request_rate, sp2_request_rate,
                 op0_expression_rate, op1_expression_rate, op2_expression_rate, op3_expression_rate, op4_expression_rate, op5_expression_rate, sp0_availability, sp1_availability, sp2_availability])
 
